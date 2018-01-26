41302bac0b2e11b39d2e3cb92f592fc0eea80ffb
feixcmeng <mingzulingzhen@163.com>
2018-01-27 02:19:27 +0800

    数据库操作

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from sign.models import Event, Guest
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    #搜索栏
+    search_fields = ['name']
+    #过滤器
+    list_filter = ['status']
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
--- sign/models.py
+++ sign/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+#发布会时间表
+class Event(models.Model):
+    name = models.CharField(max_length=100)         #发布会标题
+    limit = models.IntegerField()                   #人数
+    status = models.BooleanField()                  #发布会状态
+    address = models.CharField(max_length=200)      #地址
+    start_time = models.DateTimeField('event times')#发布会是假
+    create_time = models.DateTimeField(auto_now=True)#创建时间，自动获取
+
+    def __str__(self):
+        return self.name
+
+    #嘉宾表
+class Guest(models.Model):
+    event = models.ForeignKey(Event)            #关联发布会id
+    realname = models.CharField(max_length=64)  #姓名
+    phone = models.CharField(max_length=16)     #手机
+    email = models.EmailField()
+    sign = models.BooleanField()                #签到状态
+    create_time = models.DateField(auto_now=True) #获取创建时间
+
+    class Meta:
+        unique_together = ('event', 'phone')
+
+    def __str__(self):
+        return self.realname
