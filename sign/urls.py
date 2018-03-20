ecb0dfa02acf2296ed7b94e08d24b07eab66b952
FeiXCMeng <Weiyu.DWWC@Gmail.com>
2018-03-20 18:54:19 +0800

    Update and rename sign/urls.py to  sign/urls.py

--- sign/urls.py
+++ sign/urls.py
@@ -1,1 +1,9 @@
+from django.conf.urls import url
+from sign import views_if
 
+urlpatterns = {
+    # ex : /api/add_event
+    url(r'^add_event', views_if.add_event, name='add_event'),
+    # ex : /api/get_event_list
+    url(r'^get_event_list', views_if.get_event_list, name='get_event_list'),
+}
